83e37fd1fbe7b45a1cfd1234b9e9ce6baeadb98f
Vina del Mar <vina@duoc.acad>
2019-09-25 10:55:28 -0300

    python del mal

---
+++ pruebaPython.py
@@ -0,0 +1,20 @@
+valor = 8
+
+if valor > 0:
+    print ("mayor a 0")
+elif valor == 0:
+    print("vale 0")
+else:
+    print("Menor a 0")
+
+
+nombre = "juan"
+genero= "M"
+
+if nombre == "juan":
+    if genero == "S":
+        print ("Juan usted es hombre")
+    else:
+        print("Juan usted no es hombre")
+else:
+    print ("usted no es hombre")
